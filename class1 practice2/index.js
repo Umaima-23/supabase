1a6352cc8fe21101570cb358f5043d8abb7d8837
Umaima <umaimasohail234@gmail.com>
2025-12-14 02:05:26 +0500

    supabase

---
+++ class1 practice2/index.js
@@ -0,0 +1,22 @@
+
+import supabase from "./config.js"
+let email=document.getElementById('exampleInputEmail1')
+let password=document.getElementById('exampleInputPassword1')
+let signUp=document.getElementById('myForm')
+
+async function register(e){
+     e.preventDefault()
+    console.log(email.value,password.value)
+    try {
+        const { data, error } = await supabase.auth.signUp({
+  email: email.value,
+  password: password.value,
+})
+if(data){
+    console.log('userkaData',data)
+}
+    } catch (error) {
+        
+    }
+}
+signUp.addEventListener('submit',register)

3978e14d450e5eef72c69c768435a5ab113bdce4
Umaima <umaimasohail234@gmail.com>
2025-12-19 17:18:01 +0500

    practice image upload

--- supabase clss 3/pic.js
+++ supabase clss 3/pic.js
@@ -1,3 +1,35 @@
-import supabase from './config.js'
+import supabase from "./config.js"
 
-let img =document.getElementById('file')
+let img =document.getElementById('img')
+let uploadBtn =document.getElementById('uploadBtn')
+
+let picName;
+async function uploadMyFile() {
+    // e.preventDefault()
+    console.log(img.files[0])
+
+    let fileName = img.files[0].name
+    let file =img.files[0]
+
+const { data, error } = await supabase
+  .storage
+  .from('images')
+  .upload(Date.now()+fileName,file )
+  if(data){
+    console.log(data);
+    picName = data.path
+    const { data : urlDataa } = supabase
+  .storage
+  .from("images")
+  .getPublicUrl(picName)
+
+  if(urlDataa){
+    console.log(urlDataa.publicUrl);
+    const { error } = await supabase
+  .from('pics')
+  .insert({ id: 1, name: 'Mordor' })
+    
+  }
+  }
+}
+uploadBtn.addEventListener('click',uploadMyFile)

1efd301e3ece5c6cbe300bac1bd661818ab48d64
Umaima <umaimasohail234@gmail.com>
2025-12-25 01:57:11 +0500

    working on admin side

--- supabase project/admin dashboard/dashboard.js
+++ supabase project/admin dashboard/dashboard.js
@@ -4,5 +4,18 @@ async function checkRole() {
     const { data: { user } } = await supabase.auth.getUser()
     if(!user){
         alert('create account')
+        return window.location.href='../login.html'
+    }
+    try {
+        const { data:profile , error } = await supabase
+  .from('users')
+  .select('role')
+  .eq('userId', user.id)
+  .single()
+  console.log(profile);
+  
+
+    } catch (error) {
+        
     }
 }
